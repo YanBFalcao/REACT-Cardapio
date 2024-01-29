cdc59a6544412c6ab2c71dafa605bb7555423798
Yan Falcão <56008873+YanBFalcao@users.noreply.github.com>
2024-01-29 13:42:27 -0300

    front-end REACT - commit 29-01-2024
    front-end REACT - commit 29-01-2024

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { Card } from './components/card/card';
+import { useFoodData } from './hooks/useFoodData';
+
+function App() {
+  const { data } = useFoodData();
+
+  return (
+      <div className="container">
+        <h1>Cardápio</h1>
+        <div className="card-grid">
+          {data?.map(foodData => 
+            <Card
+              price={foodData.price}
+              title={foodData.title}
+              image={foodData.image}
+            />
+          )}
+        </div>
+      </div>
+  )
+}
+
+export default App
